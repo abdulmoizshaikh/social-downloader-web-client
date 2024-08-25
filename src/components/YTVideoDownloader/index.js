d03ded47646070d318ed8a72de26b656f1ed06c1
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2024-08-25 19:25:49 +0500

    toast integrated

--- src/components/YTVideoDownloader/index.js
+++ src/components/YTVideoDownloader/index.js
@@ -1,28 +1,47 @@
 // src/YTVideoDownloader.js
 import React, { useState } from 'react';
-import '../../App.css';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
 
 const YTVideoDownloader = () => {
   const [url, setUrl] = useState('');
   const [videoInfo, setVideoInfo] = useState(null);
 
   const getVideoInfo = async () => {
+    if (!url) {
+      toast.error('Please enter a valid YouTube URL.');
+      return;
+    }
+    
     try {
       const response = await fetch(
         `https://www.youtube.com/oembed?url=${url}&format=json`
       );
+      if (!response.ok) {
+        throw new Error('Failed to fetch video info.');
+      }
       const info = await response.json();
       setVideoInfo(info);
     } catch (error) {
       console.error('Error fetching video info:', error.message);
+      toast.error('Error fetching video info. Please try again.');
     }
   };
 
   const downloadVideo = () => {
-    if (url) {
-      window.open(
-        `http://localhost:5000/download?url=${encodeURIComponent(url)}`
-      );
+    if (!url) {
+      toast.error('Please enter a valid YouTube URL.');
+      return;
+    }
+    window.open(
+      `http://localhost:5000/download?url=${encodeURIComponent(url)}`
+    );
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      getVideoInfo();
     }
   };
 
@@ -34,6 +53,7 @@ const YTVideoDownloader = () => {
           type="text"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
         <button onClick={getVideoInfo}>Get Video Info</button>
       </div>
@@ -53,6 +73,8 @@ const YTVideoDownloader = () => {
           <button onClick={downloadVideo}>Download Video</button>
         </div>
       )}
+
+      <ToastContainer />
     </div>
   );
 };
